2735b023caab0beeb755e73c6bf37583db2ad0a5
Renzo Crisostomo <renzo@crisostomo.me>
2020-12-20 17:25:11 +0100

    Add Dec 01 solutions

---
+++ 01/P1.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def solve() -> str:
+    file = open("input.txt", "r")
+
+    expenses: List[int] = []
+    for line in file:
+        expenses.append(int(line))
+
+    expenses.sort()
+    for expense in expenses:
+        if expense > 2020:
+            continue
+        to_find = 2020 - expense
+        if to_find in expenses:
+            return to_find * expense
+
+    return "Not found"
+
+
+def main() -> None:
+    print(solve())
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01/P2.py
@@ -0,0 +1,31 @@
+from typing import List
+from itertools import islice
+
+
+def solve() -> str:
+    file = open("input.txt", "r")
+
+    expenses: List[int] = []
+    for line in file:
+        expenses.append(int(line))
+
+    expenses.sort()
+
+    for index, expense1 in enumerate(islice(expenses, 0, len(expenses) - 1)):
+        for expense2 in islice(expenses, index + 1, len(expenses)):
+            expense_so_far = expense1 + expense2
+            if expense_so_far > 2020:
+                continue
+            to_find = 2020 - expense_so_far
+            if to_find in expenses:
+                return to_find * expense1 * expense2
+
+    return "Not found"
+
+
+def main() -> None:
+    print(solve())
+
+
+if __name__ == "__main__":
+    main()
